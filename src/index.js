cad8a8d57090be09d5a721a7a4ac83c0b220d95c
yeongbba <yeongmolee2@gmail.com>
2020-10-13 17:48:57 +0900

    make login page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.module.css';
+import App from './app';
+import AuthService from './service/auth_service';
 
+const authService = new AuthService();
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App authService={authService} />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
